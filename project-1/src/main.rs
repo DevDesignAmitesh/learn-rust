20d0e1c27963486e617ee40d26066cfdee011a16
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-05-25 12:14:53 +0530

    completed guessing game

--- project-1/src/main.rs
+++ project-1/src/main.rs
@@ -1,35 +1,53 @@
-use std::io;
-
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 
 fn main() {
-    println!("Guess the number");
+    loop {
+        println!("Guess the number");
 
-    println!("Please enter your guessed number");
+        println!("Please enter your guessed number");
 
-    // generating the secret number
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("This is the secret number: {secret_number}");
-    
-    let mut guess = String::new();
-
-    // getting the guess from the users
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // generating the secret number
+        let secret_number = rand::thread_rng().gen_range(1..=100);
         
-    // converting to int so that get compared easily
-    let my_int_guess: i32 = guess
-        .trim()
-        .parse()
-        .expect("unable to convert it to number");
+        println!("This is the secret number: {secret_number}");
+
+        let mut guess = String::new();
+
+        // getting the guess from the users
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+            
         
-    if my_int_guess == secret_number {
-        println!("you guessed the right word: {guess}");
-    } else {
-        println!("you guessed the wrong word: {guess}");
+        // handling errors
+        let my_int_guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("error while parsing {err}");
+                continue;
+            },
+        };
+    
+        // the rust way
+        match my_int_guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            },
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small")
+        }
+        
+        
+        // the basic way 
+        // converting to int so that get compared easily
+        // if my_int_guess == secret_number {
+            // println!("you guessed the right word: {guess}");
+        // } else {
+            // println!("you guessed the wrong word: {guess}");
+        // }
     }
         
 }
